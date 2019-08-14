6f186967d4e63a082ee8b5a10330eb4f05ebfe47
Matias Draeger <matias.draeger@hotmail.com>
2019-08-13 22:02:00 -0300

    Ventana Nuevo CLiente

--- src/ventana/NuevoCliente.java
+++ src/ventana/NuevoCliente.java
@@ -4,8 +4,17 @@ import java.awt.EventQueue;
 
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
 
 public class NuevoCliente extends JDialog {
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
 
 	/**
 	 * Launch the application.
@@ -38,6 +47,69 @@ public class NuevoCliente extends JDialog {
 		JLabel lblCuitOCuil = new JLabel("CUIT o CUIL");
 		lblCuitOCuil.setBounds(10, 36, 91, 14);
 		getContentPane().add(lblCuitOCuil);
+		
+		JLabel lblNewLabel = new JLabel("Domicilio");
+		lblNewLabel.setBounds(10, 61, 49, 14);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblTelefono = new JLabel("Telefono");
+		lblTelefono.setBounds(10, 86, 49, 14);
+		getContentPane().add(lblTelefono);
+		
+		JLabel lblCondicionAnteEl = new JLabel("Condicion ante el IVA");
+		lblCondicionAnteEl.setBounds(10, 111, 106, 14);
+		getContentPane().add(lblCondicionAnteEl);
+		
+		JLabel lblCategora = new JLabel("Categor\u00EDa");
+		lblCategora.setBounds(10, 136, 49, 14);
+		getContentPane().add(lblCategora);
+		
+		JLabel lblPersonaResponsable = new JLabel("Persona Responsable");
+		lblPersonaResponsable.setBounds(10, 161, 125, 14);
+		getContentPane().add(lblPersonaResponsable);
+		
+		JLabel lblContactoresponsable = new JLabel("Contacto (Responsable)");
+		lblContactoresponsable.setBounds(10, 186, 125, 14);
+		getContentPane().add(lblContactoresponsable);
+		
+		textField = new JTextField();
+		textField.setBounds(193, 8, 96, 20);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(193, 33, 96, 20);
+		getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(193, 58, 96, 20);
+		getContentPane().add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(193, 83, 96, 20);
+		getContentPane().add(textField_3);
+		textField_3.setColumns(10);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(193, 133, 96, 20);
+		getContentPane().add(textField_4);
+		textField_4.setColumns(10);
+		
+		textField_5 = new JTextField();
+		textField_5.setBounds(193, 158, 96, 20);
+		getContentPane().add(textField_5);
+		textField_5.setColumns(10);
+		
+		textField_6 = new JTextField();
+		textField_6.setBounds(193, 183, 96, 20);
+		getContentPane().add(textField_6);
+		textField_6.setColumns(10);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(193, 107, 96, 22);
+		getContentPane().add(comboBox);
 
 	}
 
